19ef22e4f226a37a76c333921fa8ec45f1dd6844
kenji7157 <kawake.kk@gmail.com>
2020-12-22 01:18:50 +0900

    chap5

---
+++ study-typescript/src/chap5/example.d.ts
@@ -0,0 +1,9 @@
+export {};
+
+// list 5-2-16
+declare global {
+  namespace TempA {
+    interface RempA {}
+  }
+}
+
---
+++ study-typescript/src/chap5/example.ts
@@ -0,0 +1,11 @@
+// list5-2-12
+
+namespace Publisher {
+  export const name = ""
+  interface Appearance {
+    color: 'tempA' | 'tempB' | 'tempC'
+  }
+  export interface Book {
+
+  }
+}
