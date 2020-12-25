b716363d8e5c9f774b6cecf3c04838c8c8f6767e
kenji7157 <kawake.kk@gmail.com>
2020-12-26 02:00:06 +0900

    abc185_b

--- AtCorder/ABC/185/B/exe.js
+++ AtCorder/ABC/185/B/exe.js
@@ -1,21 +1,26 @@
 // inputに入力データ全体が入る
 function Main(input) {
 	input = input.trim().split("\n")
-	var h = input[0].split(" ")[0]
-	var w = input[0].split(" ")[1]
-	var a = [];
-	for (i=0 ;i<h; i++){
-		for (j=0 ;j<w; j++){
-			a.push(parseInt(input[i+1].split(" ")[j]));
+	var [n,m,t] = input[0].split(" ").map(x=>parseInt(x))
+	const fN = n;
+	let fB = 0;
+	for (let i=0; i<m; i++) {
+		var [a,b] = input[i+1].split(" ").map(x=>parseInt(x))
+		n = n - (a - fB);
+		if (n <= 0) {
+			console.log('No');
+			return;
 		}
+		n = n + ( b - a);
+		fB = b;
+		if (n>fN){
+			n=fN
+		}  
 	}
-	var min_val = Math.min(...a);
 
-	var c = 0;
-	a.forEach((val) => {
-		c = val >= min_val ? c + val - min_val : c;
-	})
-	console.log(c);
+	n = n - (t - b)
+
+	console.log(n>0?'Yes':'No');
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 Main(require('fs').readFileSync('./sample.txt', 'utf8'))
