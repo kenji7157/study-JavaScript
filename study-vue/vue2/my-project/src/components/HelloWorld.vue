22603c390ce41c69dcd9a46a86169a621d94f081
kenji7157 <kawake.kk@gmail.com>
2020-12-27 02:33:32 +0900

    vue

---
+++ study-vue/vue2/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input v-model="userProvidedHtml"/>
+    <div v-html="userProvidedHtml"></div>
+    <button v-on:click="send">CLICK</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      userProvidedHtml: ''
+    }
+  },
+  created: function () {
+    // alert('a');
+  },
+  methods: {
+    send: function () {
+      alert("test");
+    }
+
+  }
+  
+}
+</script>
