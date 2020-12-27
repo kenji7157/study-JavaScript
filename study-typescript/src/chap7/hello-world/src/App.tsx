ef3d135eb75ea24d3cc973338bd6a80d3fb8bb3b
kenji7157 <kawake.kk@gmail.com>
2020-12-27 17:18:20 +0900

    不要なファイルの削除

--- study-typescript/src/chap7/hello-world/src/App.tsx
+++ study-typescript/src/chap7/hello-world/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { rows } from './data/data'
 import Thead from './table/thead';
 import Tbody from './table/tbody';
-// import logo from './logo.svg';
-// import './App.css';
 
 const Component: React.FC = () => (
   <div>
@@ -14,26 +12,5 @@ const Component: React.FC = () => (
     </table>
   </div>
 );
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default Component;
