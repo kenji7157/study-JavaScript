a1692ebcc5a3683a5669b3be5baf915c805ceb1f
kenji7157 <kawake.kk@gmail.com>
2020-12-27 17:53:10 +0900

    型定義のリファクタ

--- study-typescript/src/chap7/hello-world/src/data/data.ts
+++ study-typescript/src/chap7/hello-world/src/data/data.ts
@@ -1,14 +1,24 @@
 // モックデータを生成
 export type Row = {
   id: string,
-  generation: string,
-  answers: number[]
+  age: string,
+  answers: (number | null)[]
 };
 export type Rows = Row[];
 export const rows: Rows = [
   {
     id: 'up20-un30',
-    generation: '20~30歳',
+    age: '20~30歳',
     answers: [0.18, 0.22, 0.37, 0.23]
+  },
+  {
+    id: 'up30-un40',
+    age: '30~40歳',
+    answers: [0.12, 0.28, 0.42, 0.18]
+  },
+  {
+    id: 'up40-un50',
+    age: '40~50歳',
+    answers: [null, null, null, null]
   }
 ];
