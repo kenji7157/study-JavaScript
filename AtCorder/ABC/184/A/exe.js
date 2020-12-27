9fe779354825c790f88f9118f04f2fb698ff1b9f
kenji7157 <kawake.kk@gmail.com>
2020-12-27 18:59:10 +0900

    184_A

--- AtCorder/ABC/184/A/exe.js
+++ AtCorder/ABC/184/A/exe.js
@@ -2,8 +2,9 @@
 function Main(input) {
   input = input.split("\n");
   // 入力値から最小値を抽出
-  console.log(input);
+  let [a, b] = input[0].split(" ").map((x) => parseInt(x));
+  let [c, d] = input[1].split(" ").map((x) => parseInt(x));
+  console.log(a * d - b * c);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-Main(require("fs").readFileSync("./sample.txt", "utf8"));
-// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+Main(require("fs").readFileSync("./dev/stdin", "utf8"));
