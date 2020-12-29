4f18a91017278f0acb451af105d45c534adf172b
kenji7157 <kawake.kk@gmail.com>
2020-12-29 16:54:40 +0900

    chap2 ルーティングと組み合わせたSSRの実装

--- study-nuxt/my-first-nuxt-app/pages/users/_id.vue
+++ study-nuxt/my-first-nuxt-app/pages/users/_id.vue
@@ -1,17 +1,56 @@
 <template>
-  <div>
+  <section class="container">
+    <h3>{{ user.id }}</h3>
+    <img :src="user.profile_image_url" width="120" alt="">
+    <p>{{ user.description || 'No decription' }}</p>
     <p>
-      User ID: {{ userId}}
+      <nuxt-link to="/">
+        <small>トップへ戻る</small>
+      </nuxt-link>
     </p>
-  </div>
+    <h3>{{ user.id }}さんの投稿一覧</h3>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <h4>
+          <span>{{ item.title }}</span>
+        </h4>
+        <div>{{ item.body.slice(0, 130) }}...</div>
+        <p><a target="_blank" :href="item.url">{{ item.url }}</a></p>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-  data() {
+  async asyncData({ route, app }) {
+    const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`);
+    const items =  
+      await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`);
     return {
-      userId: this.$route.params.id
+      user, items
     }
   }
 }
 </script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+</style>
