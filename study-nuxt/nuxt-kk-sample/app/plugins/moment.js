48632e463d49748bc6f70f135ea80f01ddeeba18
kenji7157 <kawake.kk@gmail.com>
2020-12-31 00:36:26 +0900

    投稿機能の実装

---
+++ study-nuxt/nuxt-kk-sample/app/plugins/moment.js
@@ -0,0 +1,4 @@
+// import 'moment/local/ja'
+import moment from 'moment'
+moment.locale('ja')
+export default moment
---
+++ study-nuxt/nuxt-kk-sample/app/store/posts.js
@@ -0,0 +1,40 @@
+import moment from '~/plugins/moment'
+
+export const state = () => ({
+  posts: [],
+})
+
+export const getters = {
+  posts: (state) => state.posts,
+}
+
+export const mutations = {
+  addPost(state, { post }) {
+    state.posts.push(post)
+  },
+  updatePost(state, { post }) {
+    // idが一致するものは引数postに更新。そうでない場合は元の値のまま
+    state.posts = state.posts.map((p) => (p.id === post.id ? post : p))
+  },
+  clearPost(state) {
+    state.posts = []
+  },
+}
+
+export const actions = {
+  async publishPost({ commit }, { payload }) {
+    const user = await this.$axios.$get(`/users/${payload.user.id}.json`)
+    // eslint-disable-next-line camelcase
+    const post_id = (await this.$axios.$post(`/posts.json`, payload)).name
+    // eslint-disable-next-line camelcase
+    const created_at = moment().format()
+    const post = { id: post_id, ...payload, created_at }
+    const putData = { id: post_id, ...payload, created_at }
+    delete putData.user
+    await this.$axios.$put(`/users/${user.id}/posts.json`, [
+      ...(user.posts || []),
+      putData,
+    ])
+    commit('addPost', { post })
+  },
+}
