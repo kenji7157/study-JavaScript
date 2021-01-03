f9685678a2bfc4cdf7a9b42c1b5a2d12bee9b02d
kenji7157 <kawake.kk@gmail.com>
2021-01-03 23:52:36 +0900

    [いいね]機能の実装

--- study-nuxt/nuxt-kk-sample/app/pages/posts/_id.vue
+++ study-nuxt/nuxt-kk-sample/app/pages/posts/_id.vue
@@ -3,11 +3,39 @@
     <div style="flex: 1">
       <el-card v-if="post">
         <div slot="header" class="clearfix">
-          <h2>{{ post.title }}</h2>
+          <h2>
+            {{ post.title }}
+          </h2>
           <small>by {{ post.user.id }}</small>
         </div>
-        <p>{{ post.body }}</p>
-        <p class="text-right">{{ post.created_at | time }}</p>
+        <p>
+          {{ post.body }}
+        </p>
+        <p class="text-right">
+          <el-button
+            v-if="isLiked"
+            :disabled="!isLoggedIn"
+            type="warning"
+            round
+            @click="unlike"
+          >
+            <span class="el-icon-star-on" />
+            <span>{{ post.likes.length }}</span>
+          </el-button>
+          <el-button
+            v-else
+            :disabled="!isLoggedIn"
+            type="warning"
+            round
+            @click="like"
+          >
+            <span class="el-icon-star-off" />
+            <span>{{ post.likes.length }}</span>
+          </el-button>
+        </p>
+        <p class="text-right">
+          {{ post.created_at | time }}
+        </p>
       </el-card>
       <p>
         <nuxt-link to="/posts">&lt; 投稿一覧へ戻る</nuxt-link>
@@ -17,7 +45,8 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import cloneDeep from 'lodash.clonedeep'
 import moment from '~/plugins/moment'
 
 export default {
@@ -27,23 +56,37 @@ export default {
     },
   },
   async asyncData({ store, route, error }) {
-    const id = route.params.id
-    if (store.getters['posts/posts'].find((p) => p.id === id)) {
+    if (store.getters['posts/posts'].find((p) => p.id === route.params.id)) {
       return
     }
-    try {
-      await store.dispatch('posts/fetchPost', { id })
-      if (!store.getters['posts/posts'].find((p) => p.id === id)) {
-        throw new Error('post not found')
-      }
-    } catch (e) {
-      error({ statusCode: 404 })
-    }
+    await store.dispatch('posts/fetchPosts')
   },
   computed: {
+    ...mapGetters(['user', 'isLoggedIn']),
     ...mapGetters('posts', ['posts']),
     post() {
       return this.posts.find((p) => p.id === this.$route.params.id)
+    },
+    isLiked() {
+      if (!this.user) return false
+      return this.post.likes.find((l) => l.user_id === this.user.id)
+    },
+  },
+  methods: {
+    ...mapActions(['addLikeLogToUser']),
+    ...mapActions('posts', ['addLikeToPost']),
+    like() {
+      if (!this.isLoggedIn) {
+        return
+      }
+      const likePayload = { user: this.user, post: this.post }
+      this.addLikeToPost(cloneDeep(likePayload))
+      this.addLikeLogToUser(cloneDeep(likePayload))
+    },
+    unlike() {
+      if (!this.isLoggedIn) {
+      }
+      // 取り消し処理は未実装
     },
   },
 }
